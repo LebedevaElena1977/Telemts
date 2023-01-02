350a096bc0d08514eb1109557f29ae5983a301ae
Elena Lebedeva <lena4lebedeva@gmail.com>
2023-01-02 15:02:10 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int addBalance = 1100;
+        int bonus = 1;
+        int addBonus;
+
+        if (addBalance >= 1000) {
+            addBonus = addBalance / 100 * bonus;
+            } else {
+            addBonus = 0;
+        }
+
+        System.out.println("Бонус: " + (balance + addBalance + addBonus));
+    }
+}
